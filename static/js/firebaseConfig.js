59f4f46403e26624d3a708be786d73ff530c0533
Leandro <leandroplus70@yahoo.com.br>
2025-11-03 12:55:14 -0300

    em fim

---
+++ static/js/firebaseConfig.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBfZDdlF1S6VPuOGIn9KUhzPpkdI4b7yAY",
+  authDomain: "frontenders-26aef.firebaseapp.com",
+  projectId: "frontenders-26aef",
+  storageBucket: "frontenders-26aef.firebasestorage.app",
+  messagingSenderId: "488255768831",
+  appId: "1:488255768831:web:59adde56462c8c39cfe4ca"
+};
